4d4c0247e433af22f875c34a6e3a7b3564c794a6
Andrew Pearson <andrew@andrewpe.com>
2017-04-09 17:16:33 -0500

    Grabbing more account info

--- main.js
+++ main.js
@@ -10,39 +10,76 @@ const WebTokenURL = 'https://webauth.southernco.com/login.aspx?WL_ReturnUrl=http
 const JWT_URL = 'https://customerservice2.southerncompany.com/Account/LogginValidated/JwtToken';
 const Monthly_Data_URL = 'https://customerservice2api.southerncompany.com/api/MyPowerUsage/MonthlyGraph';
 const Daily_Data_URL = 'https://customerservice2api.southerncompany.com/api/MyPowerUsage/DailyGraph';
+const Accounts_URL = 'https://customerservice2api.southerncompany.com/api/account/getAllAccounts';
+const ServicePointNumbers_URL = 'https://customerservice2api.southerncompany.com/api/account/servicePointNumbers/';
 
 /* Constructor */
-function SouthernCompanyAPI(config){
+function SouthernCompanyAPI(){
   /* Closure */
   const self = this;
 
-  /* Checking if data was given */
-  if(config.username == null) throw 'Username can not be left blank';
-  if(config.password == null) throw 'Password can not be left blank';
-
   /* Storing Configuration */
-  self.username = config.username;
-  self.password = config.password;
-  self.JWToken = null;
+  self.JWToken;
+  self.accounts = [];
 
-  /* Logging in to get JWToken */
-  login(self.username, self.password)
-    .then((JWToken)=>{
-      // Storing Token
-      self.JWToken = JWToken;
-
-      // Emitting event that we're logged in
-      self.emit('login');
-    });
-
-  self.printVars = ()=>{
-    console.info(`Username: ${self.username}`);
-    console.info(`Password: ${self.password}`);
-    console.info(`JWT: ${self.JWToken}`);
+  /* Debug Functions */
+  this.printVars = ()=>{
+    console.info('JWT:',self.JWToken);
+    console.info('Accounts:',self.accounts);
   };
 
   /* Public Functions */
-  self.getMonthlyData = ()=> new Promise((resolve, reject)=>{
+  this.login = (config)=> new Promise((resolve, reject)=>{
+    self.getLoginVars()
+      .then((loginVars)=>{
+        return self.getScWebToken(loginVars, config.username, config.password);
+      })
+      .then(self.getJWToken)
+      .then((JWToken)=>{
+        self.JWToken = JWToken;
+        return JWToken;
+      })
+      .then(self.getAccounts)
+      .then(self.getServicePointNumbers)
+      .then(()=>{
+        self.emit('login');
+      })
+      .then(resolve)
+      .catch(reject);
+  });
+  this.getAccounts = ()=> new Promise((resolve, reject)=>{
+    /* Setting up request */
+    const options = {
+      url: Accounts_URL,
+      method: 'GET',
+      auth:{
+        'bearer': self.JWToken
+      }
+    };
+
+    /* Making request */
+    request(options, (error, response, body)=>{
+      /* Rejecting promise on any error */
+      if(error) reject(error);
+
+      /* Parsing data */
+      const data = JSON.parse(body).Data;
+
+      /* Pulling usable data */
+      data.forEach((account)=>{
+        self.accounts.push({
+          name: account.Description,
+          primary: account.PrimaryAccount,
+          accountNumber: account.AccountNumber,
+          premiseNumber: account.PremiseNumber
+        });
+      });
+
+      /* Fulfilling Promise */
+      resolve(self.accounts);
+    });
+  });
+  this.getMonthlyData = ()=> new Promise((resolve, reject)=>{
     /* Setting up request */
     var options = {
       url: Monthly_Data_URL,
@@ -51,7 +88,7 @@ function SouthernCompanyAPI(config){
         'bearer': self.JWToken
       },
       json:{
-        'acctNum':'0000000000',
+        'accountNumber':'0000000000',
         'PremiseNo':'000000000',
         'OnlyShowCostAndUsage':'false',
         'IsWidget':'false'
@@ -81,7 +118,7 @@ function SouthernCompanyAPI(config){
       resolve(result);
     });
   });
-  self.getDailyData = (begin, end)=> new Promise((resolve, reject)=>{
+  this.getDailyData = (begin, end)=> new Promise((resolve, reject)=>{
     let startDate = moment(begin, 'M/D/Y').subtract(1, 'days');
     let endDate = moment(end, 'M/D/Y');
 
@@ -171,94 +208,128 @@ function SouthernCompanyAPI(config){
     });
   });
 
+
+  /* Semi Public Functions */
+  this.getLoginVars = ()=> new Promise((resolve, reject)=>{
+    request(LoginURL,(error, response, body)=>{
+      /* Rejecting Promise on HTTP error */
+      if(error) reject(error);
+
+      /* View State */
+      const viewstate_regex = /<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="(.*)" \/>/;
+      const viewstate_match = body.match(viewstate_regex);
+      const viewstate = viewstate_match[1];
+
+      /* Event Validation */
+      const event_validation_regex = /<input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="(.*)" \/>/;
+      const event_validation_match = body.match(event_validation_regex);
+      const event_validation = event_validation_match[1];
+
+      /* Fulfilling Promise */
+      resolve({viewstate,event_validation});
+    });
+  });
+  this.getScWebToken = (loginVars, username, password)=> new Promise((resolve, reject)=>{
+    /* Setting up request */
+    var options = {
+      url: WebTokenURL,
+      method: 'POST',
+      form: {
+        '__VIEWSTATE': loginVars.viewstate,
+        '__VIEWSTATEENCRYPTED': '',
+        '__EVENTVALIDATION': loginVars.event_validation,
+        'ctl00$MainContent$txtUsername': username,
+        'ctl00$MainContent$txtPassword': password,
+        'ctl00$MainContent$btnLogin.x': 0,
+        'ctl00$MainContent$btnLogin.y': 0
+      }
+    };
+
+    /* Making request */
+    request(options,(error, response, body)=>{
+      /* Rejecting promise on HTTP error */
+      if(error) reject(error);
+
+
+      /* Parsing out ScWebToken */
+      const ScWebToken_regex = /<INPUT TYPE='hidden' NAME='ScWebToken' value='(>.[0-9a-fA-F]+)'>/;
+      const ScWebToken_match = body.match(ScWebToken_regex);
+      const ScWebToken = ScWebToken_match[1];
+
+      /* Fulfilling Promise */
+      resolve(ScWebToken);
+    });
+
+  });
+  this.getJWToken = (ScWebToken)=> new Promise((resolve, reject)=>{
+    /* Setting up request */
+    var options = {
+      url: JWT_URL,
+      method: 'GET',
+      headers: {
+        'Cookie':`ScWebToken=${encodeURIComponent(ScWebToken)}`
+      }
+    };
+
+    /* Making Request */
+    request(options, (error, response)=>{
+      /* Rejecting promise on any error */
+      if(error) reject(error);
+
+      /* Parsing out JWT */
+      const ScJWToken_regex = /ScJwtToken=(.*);/;
+      const ScJWToken_match = response.headers['set-cookie'][0].match(ScJWToken_regex);
+      const ScJWToken = ScJWToken_match[1];
+
+      /* Fulfilling Promise */
+      resolve(ScJWToken);
+    });
+  });
+  this.getServicePointNumbers = (accounts)=> new Promise((resolve, reject)=>{
+    /* Promise holder */
+    let promises = [];
+
+    /* Looping over accounts */
+    for(let i = 0; i < accounts.length; i++){
+      promises[i] = self.getServicePointNumber(accounts[i]);
+    }
+
+    /* Waiting for all promises to complete */
+    Promise.all(promises)
+      .then((accounts)=>{
+        /* Updating stored accounts */
+        self.accounts = accounts;
+
+        /* Passing back accounts */
+        resolve(self.accounts);
+      })
+      .catch(reject);
+  });
+  this.getServicePointNumber = (account)=> new Promise((resolve, reject)=>{
+    /* Setting up request */
+    var options = {
+      url: ServicePointNumbers_URL + account.accountNumber,
+      method: 'GET',
+      auth:{
+        'bearer': self.JWToken
+      }
+    };
+
+    /* Making Request */
+    request(options, (error, response, body)=>{
+      /* Rejecting promise on any error */
+      if(error) reject(error);
+
+      const data = JSON.parse(body);
+
+      account.servicePointNumber = data.ServicePointNumber;
+      account.premiseNumber = data.PremiseNumber;
+
+      /* Fulfilling Promise */
+      resolve(account);
+    });
+  });
 }
-
-const login = (username, password)=> new Promise((resolve, reject)=>{
-  getLoginVars()
-    .then((loginVars)=>{
-      return getScWebToken(loginVars, username, password);
-    })
-    .then(getJWToken)
-    .then(resolve)
-    .catch(reject);
-});
-
-const getLoginVars = ()=> new Promise((resolve, reject)=>{
-  request(LoginURL,(error, response, body)=>{
-    /* Rejecting Promise on HTTP error */
-    if(error) reject(error);
-
-    /* View State */
-    const viewstate_regex = /<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="(.*)" \/>/;
-    const viewstate_match = body.match(viewstate_regex);
-    const viewstate = viewstate_match[1];
-
-    /* Event Validation */
-    const event_validation_regex = /<input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="(.*)" \/>/;
-    const event_validation_match = body.match(event_validation_regex);
-    const event_validation = event_validation_match[1];
-
-    /* Fulfilling Promise */
-    resolve({viewstate,event_validation});
-  });
-});
-const getScWebToken = (loginVars, username, password)=> new Promise((resolve, reject)=>{
-  /* Setting up request */
-  var options = {
-    url: WebTokenURL,
-    method: 'POST',
-    form: {
-      '__VIEWSTATE': loginVars.viewstate,
-      '__VIEWSTATEENCRYPTED': '',
-      '__EVENTVALIDATION': loginVars.event_validation,
-      'ctl00$MainContent$txtUsername': username,
-      'ctl00$MainContent$txtPassword': password,
-      'ctl00$MainContent$btnLogin.x': 0,
-      'ctl00$MainContent$btnLogin.y': 0
-    }
-  };
-
-  /* Making request */
-  request(options,(error, response, body)=>{
-    /* Rejecting promise on HTTP error */
-    if(error) reject(error);
-
-    /* Parsing out ScWebToken */
-    const ScWebToken_regex = /<INPUT TYPE='hidden' NAME='ScWebToken' value='(>.[0-9a-fA-F]+)'>/;
-    const ScWebToken_match = body.match(ScWebToken_regex);
-    const ScWebToken = ScWebToken_match[1];
-
-    /* Fulfilling Promise */
-    resolve(ScWebToken);
-  });
-
-});
-const getJWToken = (ScWebToken)=> new Promise((resolve, reject)=>{
-  /* Setting up request */
-  var options = {
-    url: JWT_URL,
-    method: 'GET',
-    headers: {
-      'Cookie':`ScWebToken=${encodeURIComponent(ScWebToken)}`
-    }
-  };
-
-  /* Making Request */
-  request(options, (error, response)=>{
-    /* Rejecting promise on any error */
-    if(error) reject(error);
-
-    /* Parsing out JWT */
-    const ScJWToken_regex = /ScJwtToken=(.*);/;
-    const ScJWToken_match = response.headers['set-cookie'][0].match(ScJWToken_regex);
-    const ScJWToken = ScJWToken_match[1];
-
-    /* Fulfilling Promise */
-    resolve(ScJWToken);
-  });
-});
-
-//login();
 
 /* Constructor */
 util.inherits(SouthernCompanyAPI, EventEmitter);
