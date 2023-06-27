f688e7c2f744a5d133610f5eda36303cbbff3978
Andrew Pearson <andrew@apearson.io>
2023-06-26 19:45:51 -0500

    Simplifying methods

--- tests/data-multiple.test.ts
+++ tests/data-multiple.test.ts
@@ -14,31 +14,9 @@ let API: SouthernCompanyAPI;
 
 /* Setting up API */
 beforeAll(() => {
-	return new Promise((resolve, reject)=> {
-		API = new SouthernCompanyAPI(config);
-		API.on('connected', resolve);
-	});
+	API = new SouthernCompanyAPI(config);
 });
 
-// test('grabs list of daily data', async ()=>{
-// 	const endDate = subDays(new Date(), 1);
-// 	const startDate = subDays(endDate, 3);
-
-// 	const data = await API.getDailyData(startDate, endDate);
-
-// 	if(!(data instanceof Array)){
-// 		throw new Error('Returned a none array');
-// 	}
-// 	else if(data.length === 0){
-// 		throw new Error('Returned an empty array');
-// 	}
-// 	else if(data.length !== config.accounts.length){
-// 		throw new Error('Returned a larger than accounts array');
-// 	}
-// 	else{
-// 		return;
-// 	}
-// });
 test('grabs list of monthly data', async ()=>{
 	const data = await API.getMonthlyData();
 
